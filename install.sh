0007d9a4117d80f2cb7c4ab06450380c942e9203
joaodanielcs <joaodanielcs@gmail.com>
2024-10-06 23:54:12 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 read -sp 'Enter the SQL Server system administrator password: ' sa_password
 echo
-apt install -y nala sudo
+apt install nala sudo -y
 clear
-nala update
+nala update -y
 nala upgrade -y
 sudo nala install gnupg2 apt-transport-https wget curl ufw neofetch expect -y
 bash -c $'echo "clear\nneofetch\systemctl list-units --type service | egrep 'apache2|SQL|ssh'" >> /etc/profile.d/mymotd.sh && chmod +x /etc/profile.d/mymotd.sh'
